a7ff337fdb6cdc83e33478a8388147300c9f6913
Joey Yu <joeyyu159357789@gmail.com>
2024-01-24 08:51:58 +0800

    finished hw3_1

---
+++ src/hw3/Hw3_1.java
@@ -0,0 +1,36 @@
+package hw3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Hw3_1 {
+    public static void main(String[] args) {
+
+        //TODO 請設計一隻程式,使用者輸入三個數字後,輸出結果會為正三角形、等腰
+        //     三角形、其它三角形或不是三角形,如圖示結果:
+
+        Scanner sc = new Scanner(System.in);
+        int[] data = new int[3];
+
+        //接收用戶輸入的資料
+        System.out.println("請輸入三個數字");
+        for (int i = 0; i < data.length; i++) {
+            data[i] = sc.nextInt();
+        }
+
+        Arrays.sort(data);
+
+        // 判斷為何種三角形
+        if (!(data[0] + data[1] > data[2])) {
+            System.out.println("不是三角形");
+        } else if (data[0] == data[1] && data[0] == data[2]) {
+            System.out.println("正三角形");
+        } else if (data[0] == data[1] || data[0] == data[2] || data[1] == data[2]) {
+            System.out.println("等腰三角形");
+        } else if (Math.pow(data[0],2) + Math.pow(data[1],2) == Math.pow(data[2],2) ) {
+            System.out.println("直角三角形");
+        } else {
+            System.out.println("其他三角形");
+        }
+    }
+}
