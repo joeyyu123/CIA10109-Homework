1e63645f6aa2079a0885fa6efa9c46b4cde56c9b
JoeyYu <joeyyu159357789@gmail.com>
2024-01-30 22:06:07 +0800

    finished hw4_2

---
+++ src/hw4/Hw4_2.java
@@ -0,0 +1,21 @@
+package hw4;
+
+import java.util.Scanner;
+
+/**
+ * 請建立一個字串，經過程式執行後，輸入結果是反過來的
+ * 例如String s = “Hello World”，執行結果即為dlroW olleH
+ * (提示：String方法，陣列)
+ */
+public class Hw4_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        sb.reverse();
+        System.out.println(sb.toString());
+
+    }
+}
